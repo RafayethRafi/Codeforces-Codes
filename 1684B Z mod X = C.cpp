a9d9e4cc3d027d571304b367a1949e7c29846be4
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:42:16 +0600

    Create 1684B Z mod X = C.cpp
    https://codeforces.com/contest/1684/problem/B

---
+++ 1684B Z mod X = C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+  long long aa,bb,cc;
+
+  cin>>aa>>bb>>cc;
+
+
+  cout<<(aa+bb+cc)<<" "<<(bb+cc)<<" "<<cc<<endl;
+
+}
+
+
+int main(){
+   int tr;
+   cin>>tr;
+   while(tr--)solve();
+
+}
