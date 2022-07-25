ab507e56a26e98af269a03ef5e31d7a26aa8c1d7
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:45:50 +0600

    Create 1686B Odd Subarrays.cpp
    https://codeforces.com/contest/1686/problem/B

---
+++ 1686B Odd Subarrays.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,cnt=0;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++) cin>>a[i];
+
+        for(i=0;i<n-1;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                cnt++;
+                i++;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
