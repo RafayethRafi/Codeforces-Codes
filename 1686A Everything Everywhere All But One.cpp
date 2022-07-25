be94e47cda3dd51695112fed6f9b37d989b93213
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:44:48 +0600

    Create 1686A Everything Everywhere All But One.cpp
    https://codeforces.com/contest/1686/problem/A

---
+++ 1686A Everything Everywhere All But One.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long sum=0;
+        int n,i;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+            sum+= a[i];
+        }
+
+        bool f=1;
+        for(i=0;i<n;i++)
+        {
+            if((sum-a[i])==((n-1)*a[i]))
+            {
+                cout<<"YES"<<endl;
+                f=0;
+                break;
+            }
+        }
+
+        if(f) cout<<"NO"<<endl;
+
+
+    }
+    return 0;
+}
