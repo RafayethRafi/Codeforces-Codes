dea7a706b909469664552a38b7decdbc70ccd111
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:47:23 +0600

    Create 1691A Beat The Odds.cpp
    https://codeforces.com/contest/1691/problem/A

---
+++ 1691A Beat The Odds.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,o=0,e=0;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+            if(a[i]%2) o++;
+            else e++;
+        }
+
+        cout<<min(o,e)<<endl;
+    }
+    return 0;
+}
