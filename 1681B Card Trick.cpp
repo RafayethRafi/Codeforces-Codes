d325be3924bffaebaef43fae9249b7f5737a84d0
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:44:09 +0600

    Create 1681B Card Trick.cpp
    https://codeforces.com/contest/1681/problem/B

---
+++ 1681B Card Trick.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++) cin>>a[i];
+        int ans = n;
+        int m;
+        cin>>m;
+        while(m--)
+        {
+            int x;
+            cin>>x;
+            ans+= x;
+            ans%= n;
+        }
+
+        cout<<a[ans]<<endl;
+
+    }
+    return 0;
+}
