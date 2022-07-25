f7a9da96a24fa04b6e658d78de98224f560624f1
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-07-25 21:35:51 +0600

    Create 1676B Equal Candies.cpp
    https://codeforces.com/contest/1676/problem/B

---
+++ 1676B Equal Candies.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++) cin>>a[i];
+        int x = *min_element(a,a+n);
+        long long sum=0;
+        for(i=0;i<n;i++)
+        {
+            sum += (a[i]-x);
+        }
+
+        cout<<sum<<endl;
+
+    }
+    return 0;
+}
