57b1f636b99d08da31fa894f54ad3aeb7d8affd9
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-08-21 01:12:54 +0600

    Create 1720B Interesting Sum.cpp
    https://codeforces.com/contest/1720/problem/B

---
+++ 1720B Interesting Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i;
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++) cin>>a[i];
+
+        sort(a,a+n);
+
+        cout<<(a[n-1]+a[n-2]-a[0]-a[1])<<endl;
+    }
+    return 0;
+}
