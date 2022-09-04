7a2ef4e786d491849de2a5a80d0617dde253c0f1
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2022-09-04 12:20:43 +0600

    Create 1717A Madoka and Strange Thoughts.cpp
    https://codeforces.com/contest/1717/problem/A

---
+++ 1717A Madoka and Strange Thoughts.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans = n+ (n/2)*2 + (n/3)*2;
+
+        cout<<ans<<endl;
+    }
+    return 0;
+}
