c4ac52d6695cbb61846e457ac801ac7fb1656f2f
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-04-01 17:41:28 +0600

    Create Round 95 (Div. 2) C. The World is a Theatre.cpp
    https://codeforces.com/contest/131/problem/C

---
+++ Round 95 (Div. 2) C. The World is a Theatre.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+#define ll long long
+
+ll NcR(int n, int r)
+{
+ 
+    // p holds the value of n*(n-1)*(n-2)...,
+    // k holds the value of r*(r-1)...
+    long long p = 1, k = 1;
+ 
+    // C(n, r) == C(n, n-r),
+    // choosing the smaller value
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            // gcd of p, k
+            long long m = __gcd(p, k);
+ 
+            // dividing by gcd, to simplify
+            // product division by their gcd
+            // saves from the overflow
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+        // k should be simplified to 1
+        // as C(n, r) is a natural number
+        // (denominator should be 1 ) .
+    }
+ 
+    else
+        p = 1;
+ 
+    // if our approach is correct p = ans and k =1
+    return p;
+}
+
+int main()
+{
+    optimize();
+    ll n,m,t;
+    cin>>n>>m>>t;
+
+    ll ans =0;
+
+    for(ll i=4;i<=n;i++)
+    {
+        ll b= i, g = t-i;
+        if(g<1) break;
+        if(g>m) continue; 
+
+        ans += NcR(n,b)*NcR(m,g);
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
